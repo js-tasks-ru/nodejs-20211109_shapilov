7ac69535dea76cb1c07513d0737594e347d8085e
Alxey Shapilov <ashapilov@spbfiller.com>
2021-11-21 15:33:01 +0300

    Solution for task: Стрим с лимитом передачи данных

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,20 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.encoding = options.encoding;
+
+    this.limitCollector = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.limitCollector += chunk.length;
 
+    if (this.limitCollector > this.limit) {
+      return callback(new LimitExceededError());
+    }
+
+    callback(null, chunk.toString(this.encoding));
   }
 }
 
