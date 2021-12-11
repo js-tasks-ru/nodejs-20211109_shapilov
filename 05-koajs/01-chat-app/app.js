92f882fa26b8a05890638b5ce01426f8c519d0cd
Alxey Shapilov <ashapilov@spbfiller.com>
2021-12-11 21:48:44 +0300

    Solution for task: Чат на Koa.js

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,49 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  const longPoll = new Promise((resolve, reject) => {
+    subscribers.push(resolve);
+    
+    ctx.res.on('error', () => {
+      subscribers.splice(subscribers.indexOf(resolve), 1);
+      const error = new Error('Connection closed');
+      error.code = 'CONNECTION_RESET';
+      reject(error);
+    })
+  });
+
+  let message;
+
+  try {
+    message = await longPoll;
+  } catch (err) {
+    if (err.code === 'CONNECTION_RESET') {
+      return;
+    }
+
+    throw err;
+  }
+
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const body = ctx.request.body;
+
+  if (!('message' in body) || body.message === '') {
+    return;
+  }
+
+  subscribers.forEach((pollResolve) => {
+    pollResolve(body.message);
+  });
+
+  subscribers = [];
+
+  ctx.body = 'OK';
 });
 
 app.use(router.routes());
